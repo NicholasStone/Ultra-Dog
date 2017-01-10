02794788274cf7367c6ab0e932963f8a41cd8a26
nicholas <1264398973@qq.com>
2017-01-10 10:43:14 +0800

    Create necessary files

---
+++ app/Http/Controllers/Frontend/Job/JobsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Frontend\Job;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PublishController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view("frontend.jobs.index");
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("frontend.jobs.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view("frontend.jobs.show");
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view("frontend.jobs.edit");
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+
+    }
+}
--- resources/views/frontend/user/account/tabs/edit.blade.php
+++ resources/views/frontend/user/account/tabs/edit.blade.php
@@ -1,25 +1,78 @@
 {{ Form::model($logged_in_user, ['route' => 'frontend.user.profile.update', 'class' => 'form-horizontal', 'method' => 'PATCH']) }}
 
+<div class="form-group">
+    {{ Form::label('name', trans('validation.attributes.frontend.name'), ['class' => 'col-md-4 control-label']) }}
+    <div class="col-md-6">
+        {{ Form::input('text', 'name', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.name')]) }}
+    </div>
+</div>
+
+@if ($logged_in_user->canChangeEmail())
     <div class="form-group">
-        {{ Form::label('name', trans('validation.attributes.frontend.name'), ['class' => 'col-md-4 control-label']) }}
+        {{ Form::label('email', trans('validation.attributes.frontend.email'), ['class' => 'col-md-4 control-label']) }}
         <div class="col-md-6">
-            {{ Form::input('text', 'name', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.name')]) }}
+            {{ Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.email')]) }}
         </div>
     </div>
-
-    @if ($logged_in_user->canChangeEmail())
-        <div class="form-group">
-            {{ Form::label('email', trans('validation.attributes.frontend.email'), ['class' => 'col-md-4 control-label']) }}
-            <div class="col-md-6">
-                {{ Form::input('email', 'email', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.email')]) }}
-            </div>
-        </div>
-    @endif
-
-    <div class="form-group">
-        <div class="col-md-6 col-md-offset-4">
-            {{ Form::submit(trans('labels.general.buttons.update'), ['class' => 'btn btn-primary', 'id' => 'update-profile']) }}
-        </div>
+@endif
+<div class="form-group">
+    {{ Form::label('name', trans('validation.attributes.frontend.tel'), ['class' => 'col-md-4 control-label']) }}
+    <div class="col-md-6">
+        {{ Form::input('text', 'tel', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.name')]) }}
     </div>
+</div>
+<div class="form-group">
+    {{ Form::label('name', trans('validation.attributes.frontend.qq'), ['class' => 'col-md-4 control-label']) }}
+    <div class="col-md-6">
+        {{ Form::input('text', 'qq', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.name')]) }}
+    </div>
+</div>
+<div class="form-group">
+    {{ Form::label('name', trans('validation.attributes.frontend.we_chat'), ['class' => 'col-md-4 control-label']) }}
+    <div class="col-md-6">
+        {{ Form::input('text', 'we_chat', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.name')]) }}
+    </div>
+</div>
+<div class="form-group">
+    {{ Form::label('name', trans('validation.attributes.frontend.real_name'), ['class' => 'col-md-4 control-label']) }}
+    <div class="col-md-6">
+        {{ Form::input('text', 'real_name', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.name')]) }}
+    </div>
+</div>
+<div class="form-group">
+    {{ Form::label('name', trans('validation.attributes.frontend.gender'), ['class' => 'col-md-4 control-label']) }}
+    <div class="col-md-6">
+        {{ Form::input('text', 'name', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.name')]) }}
+    </div>
+</div>
+<div class="form-group">
+    {{ Form::label('name', trans('validation.attributes.frontend.birthday'), ['class' => 'col-md-4 control-label']) }}
+    <div class="col-md-6">
+        {{ Form::input('text', 'name', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.name')]) }}
+    </div>
+</div>
+<div class="form-group">
+    {{ Form::label('name', trans('validation.attributes.frontend.id_number'), ['class' => 'col-md-4 control-label']) }}
+    <div class="col-md-6">
+        {{ Form::input('text', 'id_number', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.name')]) }}
+    </div>
+</div>
+<div class="form-group">
+    {{ Form::label('name', trans('validation.attributes.frontend.resume'), ['class' => 'col-md-4 control-label']) }}
+    <div class="col-md-6">
+        {{ Form::textarea('text', 'resume', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.name')]) }}
+    </div>
+</div>
+<div class="form-group">
+    {{ Form::label('name', trans('validation.attributes.frontend.avatar'), ['class' => 'col-md-4 control-label']) }}
+    <div class="col-md-6">
+        {{ Form::input('file', 'avatar', null, ['class' => 'form-control', 'placeholder' => trans('validation.attributes.frontend.name')]) }}
+    </div>
+</div>
+<div class="form-group">
+    <div class="col-md-6 col-md-offset-4">
+        {{ Form::submit(trans('labels.general.buttons.update'), ['class' => 'btn btn-primary', 'id' => 'update-profile']) }}
+    </div>
+</div>
 
 {{ Form::close() }}
