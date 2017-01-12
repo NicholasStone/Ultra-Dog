f25634fe953b46a2b8893b76bcdeca157fadbb90
nicholas <1264398973@qq.com>
2017-01-12 10:56:16 +0800

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-import elixir from "laravel-elixir";
-import laravel_elixir_webpack_ex from "laravel-elixir-webpack-ex";
-import laravel_elixir_vue_2 from "laravel-elixir-vue-2";
+const elixir = require('laravel-elixir');
 
+require('laravel-elixir-vue-2');
+require('./elixir-extensions');
 
 /*
  |--------------------------------------------------------------------------
@@ -23,88 +23,100 @@ elixir((mix) => {
         'node_modules/font-awesome/fonts',
         'public/build/fonts/font-awesome'
     )
-    .copy(
-        'node_modules/bootstrap-sass/assets/fonts/bootstrap',
-        'public/build/fonts/bootstrap'
-    )
+        .copy(
+            'node_modules/bootstrap-sass/assets/fonts/bootstrap',
+            'public/build/fonts/bootstrap'
+        )
 
-    /**
-     * Process frontend SCSS stylesheets
-     */
-    .sass([
-        'frontend/app.scss',
-        'plugin/sweetalert/sweetalert.scss'
-    ], 'resources/assets/css/frontend/app.css')
+        /**
+         * Process frontend SCSS stylesheets
+         */
+        .sass([
+            'frontend/app.scss',
+            'plugin/sweetalert/sweetalert.scss'
+        ], 'resources/assets/css/frontend/app.css')
 
-    /**
-     * Combine pre-processed frontend CSS files
-     */
-    .styles([
-        'frontend/app.css'
-    ], 'public/css/frontend.css')
+        /**
+         * Combine pre-processed frontend CSS files
+         */
+        .styles([
+            'frontend/app.css'
+        ], 'public/css/frontend.css')
 
-    /**
-     * Pack it up
-     * Saves to a dist folder in resources, it is then combined and placed in public
-     */
-    .webpack('frontend/app.js', './resources/assets/js/dist/frontend.js')
+        /**
+         * Pack it up
+         * Saves to a dist folder in resources, it is then combined and placed in public
+         */
+        .webpack('frontend/app.js', './resources/assets/js/dist/frontend.js')
 
-    /**
-     * Combine frontend scripts
-     */
-    .scripts([
-        'dist/frontend.js',
-        'plugin/sweetalert/sweetalert.min.js',
-        'plugins.js'
-    ], 'public/js/frontend.js')
+        /**
+         * Combine frontend scripts
+         */
+        .scripts([
+            'dist/frontend.js',
+            'plugin/sweetalert/sweetalert.min.js',
+            'plugins.js'
+        ], 'public/js/frontend.js')
 
-    /**
-     * Process backend SCSS stylesheets
-     */
-    .sass([
-        'backend/app.scss',
-        'plugin/toastr/toastr.scss',
-        'plugin/sweetalert/sweetalert.scss'
-    ], 'resources/assets/css/backend/app.css')
+        /**
+         * Process backend SCSS stylesheets
+         */
+        .sass([
+            'backend/app.scss',
+            'plugin/toastr/toastr.scss',
+            'plugin/sweetalert/sweetalert.scss'
+        ], 'resources/assets/css/backend/app.css')
 
-    /**
-     * Combine pre-processed backend CSS files
-     */
-    .styles([
-        'backend/app.css'
-    ], 'public/css/backend.css')
+        /**
+         * Combine pre-processed backend CSS files
+         */
+        .styles([
+            'backend/app.css'
+        ], 'public/css/backend.css')
 
-    /**
-     * Pack it up
-     * Saves to a dist folder in resources, it is then combined and placed in public
-     */
-    .webpack('backend/app.js', './resources/assets/js/dist/backend.js')
+        /**
+         * Pack it up
+         * Saves to a dist folder in resources, it is then combined and placed in public
+         */
+        .webpack('backend/app.js', './resources/assets/js/dist/backend.js')
 
-    /**
-     * Combine backend scripts
-     */
-    .scripts([
-        'dist/backend.js',
-        'plugin/sweetalert/sweetalert.min.js',
-        'plugin/toastr/toastr.min.js',
-        'plugins.js',
-        'backend/custom.js'
-    ], 'public/js/backend.js')
+        /**
+         * Make RTL (Right To Left) CSS stylesheet for the backend
+         */
+        .rtlCss()
 
-    /**
-     * Apply version control
-     */
-    .version([
-        "public/css/frontend.css",
-        "public/js/frontend.js",
-        "public/css/backend.css",
-        "public/css/backend-rtl.css",
-        "public/js/backend.js",
-        "public/css/rtl.css"
-    ])
+        /**
+         * Combine backend scripts
+         */
+        .scripts([
+            'dist/backend.js',
+            'plugin/sweetalert/sweetalert.min.js',
+            'plugin/toastr/toastr.min.js',
+            'plugins.js',
+            'backend/custom.js'
+        ], 'public/js/backend.js')
 
-    /**
-     * Run tests
-     */
-    .phpUnit();
+        /**
+         * Combine pre-processed rtl CSS files
+         */
+        .styles([
+            'rtl/bootstrap-rtl.css'
+        ], 'public/css/rtl.css')
+
+        /**
+         * Apply version control
+         */
+        .version([
+            "public/css/frontend.css",
+            "public/js/frontend.js",
+            "public/css/backend.css",
+            "public/css/backend-rtl.css",
+            "public/js/backend.js",
+            "public/css/rtl.css"
+        ])
+
+        /**
+         * Run tests
+         */
+        .phpUnit();
 });
