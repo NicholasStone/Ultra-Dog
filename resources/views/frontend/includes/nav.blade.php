fe6f6f865533aafa9e2427bd0f44dd398ffd3a9d
nicholas <1264398973@qq.com>
2017-01-13 14:22:54 +0800

    add logo

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -9,7 +9,7 @@
                 <span class="icon-bar"></span>
             </button>
 
-            {{ link_to_route('frontend.index', app_name(), [], ['class' => 'navbar-brand']) }}
+            <img src="img/frontend/logo.png" alt="logo" class="navbar-brand">{{ link_to_route('frontend.index', app_name(), [], ['class' => 'navbar-brand']) }}
         </div><!--navbar-header-->
 
         <div class="collapse navbar-collapse" id="frontend-navbar-collapse">
